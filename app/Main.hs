864a0cf694a863c5aedd67744ee4f6b4c50fb6f8
Qiao Wang <wangqiao11@hotmail.com>
2022-07-16 23:07:42 +0800

    Add args parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,50 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
+import Options.Applicative
+    ( (<**>),
+      idm,
+      info,
+      prefs,
+      showHelpOnEmpty,
+      customExecParser,
+      helper )
 
-import Stargaze.CLI (loadAllProjects)
+import Stargaze.Types ( Config(Config, cfgUser) )
+import Stargaze.Command
+    ( parseCommand,
+      Command(ListOwners, SetConfig, UpdateProjects, ListTags) )
+import Stargaze.CLI
+    ( loadConfig,
+      loadLocalProjects,
+      showTopOwners,
+      showTopTags,
+      updateLocalProjects,
+      writeConfig )
 
 main :: IO ()
-main = loadAllProjects "nodew"
+main = do
+    options <- customExecParser p opts
+    exec options
+    where
+        opts = info (parseCommand <**> helper) idm
+        p = prefs showHelpOnEmpty
+
+exec :: Command -> IO ()
+
+exec (SetConfig user) = loadConfig >>= \case
+    Left err -> writeConfig $ Config user Nothing
+    Right config -> writeConfig $ config { cfgUser = user }
+
+exec UpdateProjects = updateLocalProjects
+
+exec ListTags = loadLocalProjects >>= \case
+    Left err -> putStrLn err
+    Right projects -> showTopTags 20 projects
+
+exec ListOwners = loadLocalProjects >>= \case
+    Left err -> putStrLn err
+    Right projects -> showTopOwners 20 projects
+
+exec _ = putStrLn "Not implement yet"
---
+++ src/Stargaze/Aggregate.hs
@@ -0,0 +1,58 @@
+module Stargaze.Aggregate
+( aggregate
+, MergeMap(..)
+, ProjectAgg(..)
+) where
+
+import Data.HashMap ( Map )
+import Data.Hashable ( Hashable )
+import qualified Data.HashMap as HashMap
+import Stargaze.Types ( Author (authorId), Project (projectOwner, projectTopics) )
+import qualified Data.Aeson.KeyMap as HasMap
+import Data.Int ( Int64 )
+import Data.String (IsString(fromString))
+import Data.Foldable (foldl')
+import Data.Function (on)
+
+newtype MergeMap k v = MergeMap { getMergeMap :: Map k v } deriving (Show)
+
+instance (Semigroup v, Eq k, Hashable k, Ord k) => Semigroup (MergeMap k v) where
+    MergeMap x <> MergeMap y = MergeMap $ HashMap.unionWith (<>) x y
+
+instance (Semigroup v, Eq k, Hashable k, Ord k) => Monoid (MergeMap k v) where
+    mempty = MergeMap mempty
+    mappend = (<>)
+
+data ProjectAgg = ProjectAgg
+    { aggByTag   :: MergeMap String [Project]
+    , aggByOwner :: MergeMap Int64 [Project]
+    , aggAuthor  :: Map Int64 Author
+    } deriving (Show)
+
+instance Semigroup ProjectAgg where
+  x <> y =
+    ProjectAgg
+        { aggByTag = on (<>) aggByTag x y
+        , aggByOwner = on (<>) aggByOwner x y
+        , aggAuthor = on (<>) aggAuthor x y
+        }
+
+instance Monoid ProjectAgg where
+  mappend = (<>)
+  mempty = ProjectAgg mempty mempty mempty
+
+singletonAggregate :: Project -> ProjectAgg
+singletonAggregate project =
+    ProjectAgg
+        { aggByTag = foldl' (<>) mempty _singletonTags
+        , aggByOwner = MergeMap $ HashMap.singleton _ownerId [project]
+        , aggAuthor = HashMap.singleton _ownerId _owner
+        }
+    where
+        _owner = projectOwner project
+        _ownerId = authorId _owner
+        _tags = projectTopics project
+        _singletonTags = map (\tag -> MergeMap $ HashMap.singleton tag [project]) _tags
+
+aggregate :: [Project] -> ProjectAgg
+aggregate = foldl' (\acc project -> acc <> singletonAggregate project) mempty
